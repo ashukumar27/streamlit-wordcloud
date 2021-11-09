a1e27c417a7c69e2a1a5429beea202653f7172a5
Ashutosh Kumar <ashu.iitkgp@gmail.com>
2021-11-09 18:47:55 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -23,24 +23,41 @@ st.sidebar.header("Configuration Options")
 
 df = pd.read_csv('data.csv')
 
-st.write(df.head())
+word_cloud_data = df['Reason_to_Visit_Cleaned']
 
+long_string = ','.join(list(word_cloud_data.values))
+long_string=long_string.replace('nan', '')
 
+# WordCloud object
+wordcloud2 = WordCloud(background_color="black", max_words=5000,  width=1600, height=800, max_font_size=200)
+# Generate a word cloud
+wordcloud2.generate(long_string)
 
-
-
-
-
-
-
-text = 'Fun, fun, awesome, awesome, tubular, astounding, superb, great, amazing, amazing, amazing, amazing'
-
-# Create and generate a word cloud image:
-wordcloud = WordCloud().generate(text)
-
-# Display the generated image:
-plt.imshow(wordcloud, interpolation='bilinear')
+plt.figure( figsize=(20,10), facecolor='k')
+plt.imshow(wordcloud2, interpolation='bilinear')
 plt.axis("off")
 plt.show()
 st.pyplot()
+
+
+#st.write(df.head())
+
+
+
+
+
+
+
+
+
+# text = 'Fun, fun, awesome, awesome, tubular, astounding, superb, great, amazing, amazing, amazing, amazing'
+
+# # Create and generate a word cloud image:
+# wordcloud = WordCloud().generate(text)
+
+# # Display the generated image:
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis("off")
+# plt.show()
+# st.pyplot()
 
